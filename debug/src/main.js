77b691712bcc7dd32a78bf92a57aa98f3665c184
Mukedlii <mukedlii@local>
2026-02-28 17:34:02 +0100

    chore: add debug/ actor scaffold

---
+++ debug/src/main.js
@@ -0,0 +1,4 @@
+// TODO: ide kell bemásolni a hasznaltauto_DEBUG_v2.zip-ből a teljes src/main.js tartalmát.
+// Küldd el a hasznaltauto_DEBUG_v2 zip-et és beteszem + push.
+console.log('hasznaltauto-debug: missing main.js (please provide hasznaltauto_DEBUG_v2.zip)');
+process.exit(1);
